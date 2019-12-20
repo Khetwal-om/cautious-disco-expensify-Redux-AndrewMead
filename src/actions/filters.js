85276d0693430631eb02bdb4256e11679803fa2b
Khetwal-om <36092598+Khetwal-om@users.noreply.github.com>
2019-12-20 11:02:49 +0530

    01 reducers,actions,expenses defined

---
+++ src/actions/filters.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+// SET_TEXT_FILTER
+
+export const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+// SORT BY DATE
+
+export const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+
+// SORT_BY_AMOUNT
+
+export const sortByAmount = () => ({
+    type: 'SORT_BY_'
+});
+
+
